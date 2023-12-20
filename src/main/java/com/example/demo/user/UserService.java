3c5544261e828672df5a4cf3d8d52110fd044a6b
JOSSE Clément <josse@et.esiea.fr>
2023-12-20 14:58:18 +0100

    GET all users fonctionnel

--- src/main/java/com/example/demo/user/UserService.java
+++ src/main/java/com/example/demo/user/UserService.java
@@ -1,5 +1,6 @@
 package com.example.demo.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
@@ -9,16 +10,15 @@ import java.util.List;
 
 @Service
 public class UserService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository){
+        this.userRepository= userRepository;
+    }
+
     public List<User> GetUsers(){
-        return List.of(
-                new User("test@gmail.com",
-                        "password",
-                        "name",
-                        "fistname",
-                        "phone",
-                        "address",
-                        "ZIP",
-                        LocalDate.of(2000, Month.JANUARY,5))
-        );
+        return userRepository.findAll();
     }
 }
